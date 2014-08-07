907e1ba26dcd74c1c6d97ecd358655a32629aaa1
samuelboko <samuel.bokobza@gmail.com>
2014-08-07 11:36:36 +0200

    new_branch

---
+++ public/assets/js/functions.js
@@ -0,0 +1,56 @@
+var map;
+var panel;
+var initialize;
+var calculate;
+var direction;
+
+initialize = function(){
+  var latLng = new google.maps.LatLng(50.6371834, 3.063017400000035); // Correspond au coordonnées de Lille
+  var myOptions = {
+    zoom      : 14, // Zoom par défaut
+    center    : latLng, // Coordonnées de départ de la carte de type latLng 
+    mapTypeId : google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
+    maxZoom   : 20
+  };
+  
+  map      = new google.maps.Map(document.getElementById('map'), myOptions);
+  panel    = document.getElementById('panel');
+  
+  var marker = new google.maps.Marker({
+    position : latLng,
+    map      : map,
+    title    : "Lille"
+    //icon     : "marker_lille.gif" // Chemin de l'image du marqueur pour surcharger celui par défaut
+  });
+  
+
+  
+  
+  direction = new google.maps.DirectionsRenderer({
+    map   : map,
+    panel : panel // Dom element pour afficher les instructions d'itinéraire
+  });
+
+};
+
+calculate = function(){
+    origin      = document.getElementById('origin').value; // Le point départ
+    destination = document.getElementById('destination').value; // Le point d'arrivée
+    travelMode = document.getElementById('transport').value; // Le moyen de transport
+    if(origin && destination){
+        var request = {
+            origin      : origin,
+            destination : destination,
+            travelMode  : travelMode // Mode de conduite
+            //travelMode  : google.maps.DirectionsTravelMode.DRIVING // Mode de conduite
+        }
+        var directionsService = new google.maps.DirectionsService(); // Service de calcul d'itinéraire
+        directionsService.route(request, function(response, status){ // Envoie de la requête pour calculer le parcours
+            if(status == google.maps.DirectionsStatus.OK){
+                direction.setDirections(response); // Trace l'itinéraire sur la carte et les différentes étapes du parcours
+            }
+        });
+    }
+};
+
+initialize();
