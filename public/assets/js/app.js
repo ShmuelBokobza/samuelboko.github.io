2d7e4e06fa2cfd7886c4d1320613d2d64e2e31a3
samuelboko <samuel.bokobza@gmail.com>
2014-08-30 20:11:39 +0200

    New Design

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,8 @@
-$(function () {
+(function(){
+	
+}());
 
-	/* Scrolling top menu */
+/* Scrolling top menu 
 	$(document).on('scroll', function(e){
 		if($(document).scrollTop() > 80){
 			$('#header').addClass('header-mini')
@@ -8,90 +10,4 @@ $(function () {
 			$('#header').removeClass('header-mini')
 		};
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var $window     = $(window);
-var header      = $('#header');
-var headerLogo  = $('.header-logo');
-var headerTitre = $('.header-titre');
-var headerLogin = $('.header-login');
-var block       = $('.container-block-elem');
-var blockB      = $('.container-block-elem .btn');
-var side        = $('#aside');
-
-
-side.on('click', function() {
-	$('.badge').velocity("fadeIn", {
-	}, "linear");
-})
-
-blockB.on('mouseover', function() {
-	blockB.velocity("fadeIn", {
-		duration:100
-	}, "linear");
-})
-
- $window.scroll(function () {
-	if ($window.scrollTop() == 0){
-		console.log('Scroll en haut de page');
-		header
-			.velocity({
-				translateY:"0px"
-			})
-			headerLogo
-			.velocity({
-				translateY:"0px"
-			})
-		headerTitre
-			.velocity({
-				translateY:"0px"
-			})
-
-		headerLogin
-			.velocity({
-				translateY:"26px"
-			})
-
-	}else{
-		console.log('Scroll en milieu de page');
-		
-		header
-			.velocity({
-				translateY:"-26%"
-			})
-			headerLogo
-			.velocity({
-				translateY:"52px"
-			})
-		headerTitre
-			.velocity({
-				translateY:"80px"
-			})
-
-		headerLogin
-			.velocity({
-				translateY:"180px"
-			})
-
-	}
-
-});*/
-
-
-
-});
+*/
